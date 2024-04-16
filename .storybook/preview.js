a454262cfe68286e9e1a71a48a8e330b7d0228f9
AldreiMiranda <aldrei_miranda@hotmail.com>
2024-04-16 17:55:33 -0300

    feat: add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import '../src/index.css';
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+   
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
